6707a83f82b4ff756988ee3655b19a59e20afbfb
Alexey <ronski941@gmail.com>
2020-02-02 20:59:23 +0300

    added new selectors

--- pages/locators.py
+++ pages/locators.py
@@ -4,3 +4,14 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     PAGE_LINK = 'http://selenium1py.pythonanywhere.com/'
+
+
+class LoginPageLocators():
+    LOG_EMAIL_FORM = (By.CSS_SELECTOR, '#id_login-username')
+    LOG_PASSWORD_FORM = (By.CSS_SELECTOR, '#id_login-password')
+    LOG_BUTTON_FORM = (By.CSS_SELECTOR, '[name=login_submit]')
+    REG_EMAIL_FORM = (By.CSS_SELECTOR, '#id_registration-email')
+    REG_PASSWORD_FORM = (By.CSS_SELECTOR, '#id_registration-password1')
+    REG_PASSWORD_CONFIRM_FORM = (By.CSS_SELECTOR, '#id_registration-password2')
+    REG_BUTTON_FORM = (By.CSS_SELECTOR, '[name=registration_submit]')
+    
