5089dec59e71ef577d922f2b76153d98b1ce7daa
Alexey <ronski941@gmail.com>
2020-02-02 23:52:45 +0300

    added test for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
-from .pages.locators import MainPageLocators
+from .pages.locators import MainPageLocators, LoginPageLocators
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -14,3 +15,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+def test_should_be_login_page(browser):
+    link = LoginPageLocators.PAGE_LINK
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_page()
