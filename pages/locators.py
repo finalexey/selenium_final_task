a741eff08e508fa3e2cf2676710e1a07dd892222
Alexey <ronski941@gmail.com>
2020-02-02 20:13:51 +0300

    added locators script

---
+++ pages/locators.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    PAGE_LINK = 'http://selenium1py.pythonanywhere.com/'
