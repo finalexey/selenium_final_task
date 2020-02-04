d24e84f81a9d5cb72a0bb1030243cd777b019bec
Alexey <ronski941@gmail.com>
2020-02-04 21:39:36 +0300

    added two test methods

--- test_product_page.py
+++ test_product_page.py
@@ -8,7 +8,6 @@ from .pages.product_page import ProductPage
 
 
 #@pytest.mark.parametrize('offer', link_offers)
-@pytest.mark.skip
 def test_guest_can_add_product_to_basket(browser):
     link = ProductPageLocators.PAGE_LINK
     # link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}'
@@ -20,6 +19,7 @@ def test_guest_can_add_product_to_basket(browser):
     page.should_be_same_prices_in_basket_and_notification()
 
 
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = ProductPageLocators.PAGE_LINK
     page = ProductPage(browser, link)
@@ -35,6 +35,7 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = ProductPageLocators.PAGE_LINK
     page = ProductPage(browser, link)
@@ -42,3 +43,17 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_be_adding_product_to_basket()
     time.sleep(1)
     page.should_be_disappeared()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
