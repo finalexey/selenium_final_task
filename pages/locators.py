2c294cfd113b64b8d041d97676cb4e5423161aee
Alexey <ronski941@gmail.com>
2020-02-04 15:28:39 +0300

    added product_page locators

--- pages/locators.py
+++ pages/locators.py
@@ -15,3 +15,12 @@ class LoginPageLocators:
     REG_PASSWORD_FORM = (By.CSS_SELECTOR, '#id_registration-password1')
     REG_PASSWORD_CONFIRM_FORM = (By.CSS_SELECTOR, '#id_registration-password2')
     REG_BUTTON_FORM = (By.CSS_SELECTOR, '[name=registration_submit]')
+
+
+class ProductPageLocators:
+    PAGE_LINK = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+    NAME_OF_PRODUCT = (By.CSS_SELECTOR, '.product_main h1')
+    PRODUCT_PRICE_MAIN = (By.CSS_SELECTOR, '.product_main .price_color')
+    NAME_IN_NOTIFICATION = (By.CSS_SELECTOR, '#messages :first-child > div > strong')
+    PRODUCT_PRICE_NOTIFICATION = (By.CSS_SELECTOR, '.alertinner > p > strong')
