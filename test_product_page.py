f3e40c64dac0fe7fe1ec72f00ff32828106bef10
Alexey <ronski941@gmail.com>
2020-02-04 16:24:52 +0300

    added parametrize to link

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,15 @@
+import pytest
 from .pages.locators import ProductPageLocators
 from .pages.product_page import ProductPage
 
+link_offers = ['0', '1', '2', '3', '4', '5', '6', pytest.param('7', marks=pytest.mark.xfail), '8', '9']
 
-def test_guest_can_add_product_to_basket(browser):
 
-    link = ProductPageLocators.PAGE_LINK
+@pytest.mark.parametrize('offer', link_offers)
+def test_guest_can_add_product_to_basket(browser, offer):
+
+    # link = ProductPageLocators.PAGE_LINK
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}'
     page = ProductPage(browser, link)
     page.open()
     page.should_be_adding_product_to_basket()
