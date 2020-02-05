5351998a182329ee7eab38550095f7707204714b
Alexey <ronski941@gmail.com>
2020-02-05 21:34:56 +0300

    added new test for basket page

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,8 @@
 import time
-
 import pytest
-from .pages.locators import ProductPageLocators
+from .pages.locators import ProductPageLocators, MainPageLocators
 from .pages.product_page import ProductPage
+from .pages.basket_page import BasketPage
 
 #link_offers = ['0', '1', '2', '3', '4', '5', '6', pytest.param('7', marks=pytest.mark.xfail), '8', '9']
 
@@ -57,3 +57,12 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = MainPageLocators.PAGE_LINK
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_not_be_items_in_basket()
+    page.should_be_text_of_empty_basket()
