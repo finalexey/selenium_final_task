d52480b67ebd6f3de8dc1860a466e41fb36b1574
Alexey <ronski941@gmail.com>
2020-02-05 21:35:32 +0300

    added new test for basket page

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 from .pages.locators import BasePageLocators, LoginPageLocators
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
+from .pages.basket_page import BasketPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -24,3 +25,12 @@ def test_should_be_login_page(browser):
     page = LoginPage(browser, link)
     page.open()
     page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/'
+    page = BasketPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    page.should_not_be_items_in_basket()
+    page.should_be_text_of_empty_basket()
